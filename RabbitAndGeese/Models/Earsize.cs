a206a5e156b8afe5a473c94381626c1dae730d45
Amanda Slayton <alslayton86@gmail.com>
2018-09-17 21:19:41 -0500

    classwork

---
+++ RabbitAndGeese/Models/Earsize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitAndGeese.Models
+{
+    //ctrl + . will move class to new thingie?
+    public enum Earsize
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
---
+++ RabbitAndGeese/Models/Rabbit.cs
@@ -0,0 +1,17 @@
+﻿using RabbitAndGeese.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitAndGeese
+{
+    public class Rabbit
+    {
+        public string Color { get; set; }
+        public int MaxFeetPerSecond { get; set; }
+        //Enum represented by integers by default
+        public Earsize Earsize { get; set; }
+        public Sex Sex {get; set; }
+    }
+}
