848b8bddc027d5751aec4a809fd85917e0a8ebe5
Amanda Slayton <alslayton86@gmail.com>
2018-09-18 21:30:27 -0500

    notes

--- RabbitAndGeese/Models/Rabbit.cs
+++ RabbitAndGeese/Models/Rabbit.cs
@@ -8,10 +8,14 @@ namespace RabbitAndGeese
 {
     public class Rabbit
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
         public string Color { get; set; }
         public int MaxFeetPerSecond { get; set; }
         //Enum represented by integers by default
-        public Earsize Earsize { get; set; }
+        public Size Earsize { get; set; }
         public Sex Sex {get; set; }
+        public List<Goose> OwnedGeese { get; set; } = new List<Goose>();
+        public List<Saddle> OwnedSaddles { get; set; } = new List<Saddle>();
     }
 }
---
+++ RabbitAndGeese/Models/SaddleType.cs
@@ -0,0 +1,9 @@
+﻿public enum SaddleType
+{
+    War,
+    Speed,
+    Yoga,
+    Karate,
+    Luxury,
+    Party
+}
